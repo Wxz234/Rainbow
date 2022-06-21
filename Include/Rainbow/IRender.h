cdac09396992d28bbaa438063bc8a5f8ddb4f66a
Wxz <429373960@qq.com>
2022-06-22 04:32:25 +0800

    add Copyright header

---
+++ Include/Rainbow/IRender.h
@@ -0,0 +1,19 @@
+//
+// Copyright (c) Wxz.
+// Licensed under the MIT License.
+//
+// https://github.com/Wxz234/Rainbow
+//
+
+#pragma once
+
+#include <d3d12.h>
+
+namespace Rainbow {
+	enum PipelineType
+	{
+		PIPELINE_TYPE_GRAPHICS = 0,
+		PIPELINE_TYPE_COMPUTE,
+		PIPELINE_TYPE_COUNT,
+	};
+}
