f73bccf90e15cf6b3ec531d77642fc468a3da62f
Wxz <429373960@qq.com>
2022-06-22 06:29:43 +0800

    Update SubMesh.h

---
+++ Include/Rainbow/Mesh/SubMesh.h
@@ -0,0 +1,17 @@
+//
+// Copyright (c) Wxz.
+// Licensed under the MIT License.
+//
+// https://github.com/Wxz234/Rainbow
+//
+
+#pragma once
+
+#include "../Math/Math.h"
+
+#include <vector>
+
+namespace Rainbow {
+	struct SubMesh {
+	};
+}
