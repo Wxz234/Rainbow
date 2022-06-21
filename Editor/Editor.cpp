1734c59863c24edf016050677e942f7b49f1d62d
Wxz <429373960@qq.com>
2022-06-22 05:22:49 +0800

    add unicode

---
+++ Editor/Editor.cpp
@@ -0,0 +1,15 @@
+//
+// Copyright (c) Wxz.
+// Licensed under the MIT License.
+//
+// https://github.com/Wxz234/Rainbow
+//
+
+#include "Rainbow/Window/Window.h"
+
+#include <Windows.h>
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	return 0;
+}
