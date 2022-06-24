bdb770708c38c4bb774333a7c6cc8e70cf066393
Wxz <429373960@qq.com>
2022-06-24 23:15:25 +0800

    Update Editor.cpp

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -7,10 +7,39 @@
 
 #include "Rainbow/Renderer/IRenderer.h"
 #include "Rainbow/Math/Math.h"
+
 #include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 {
+	uint32_t width = 800, height = 600;
+
+	WNDCLASSEXW wcex = {};
+	wcex.cbSize = sizeof(WNDCLASSEXW);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.hInstance = GetModuleHandle(nullptr);
+	wcex.hIcon = LoadIconW(wcex.hInstance, L"IDI_ICON");
+	wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+	wcex.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+	wcex.lpszClassName = L"RainbowEditor";
+	wcex.hIconSm = LoadIconW(wcex.hInstance, L"IDI_ICON");
+	RegisterClassExW(&wcex);
+
+
 	Rainbow::Device* pDevice = nullptr;
 	Rainbow::CreateDevice(&pDevice);
 	Rainbow::TextureDesc desc{ {}, DXGI_FORMAT_R8G8B8A8_UNORM, 800, 600, 1, 1, D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_FLAG_NONE };
@@ -19,5 +48,19 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _
 
 	Rainbow::RemoveTexture(pTexture);
 	Rainbow::RemoveDevice(pDevice);
+
+	RECT rc = { 0, 0, static_cast<LONG>(width), static_cast<LONG>(height) };
+	auto stype = WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX;
+	AdjustWindowRect(&rc, stype, FALSE);
+	auto hwnd = CreateWindowExW(0, L"RainbowEditor", L"RainbowEditor", stype, 50, 50, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, GetModuleHandle(nullptr), nullptr);
+	ShowWindow(hwnd, SW_SHOWDEFAULT);
+	MSG msg{};
+	while (msg.message != WM_QUIT) {
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		//std::invoke(std::forward<Function>(f), std::forward<Args>(args)...);
+	}
 	return 0;
 }
