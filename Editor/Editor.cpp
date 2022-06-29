5c41c0a453f8cea93b4994b388352b53a303492f
Wxz <429373960@qq.com>
2022-06-30 02:42:38 +0800

    fix bug

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -9,7 +9,7 @@
 #include "../Include/Rainbow/Renderer/IRenderer.h"
 
 #include <Windows.h>
-
+uint32_t w = 800, h = 600;
 constexpr uint32_t frameCount = 3;
 Rainbow::Device* pDevice = nullptr;
 Rainbow::SwapChain* pSwapChain = nullptr;
@@ -23,6 +23,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_SIZE:
+		w = LOWORD(lParam);
+		h = HIWORD(lParam);
+		Rainbow::QueueWait(pDevice->pQueue);
+		Rainbow::SwapChainResize(pSwapChain, w, h, DXGI_FORMAT_UNKNOWN);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -35,14 +39,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 {	
-	uint32_t w = 800, h = 600;
 	Rainbow::Window* pWindow = nullptr;
 	Rainbow::CreateRenderWindow("RainbowEditor", w, h, WndProc, &pWindow);
-	Rainbow::RenderWindowShow(pWindow);
 
 	Rainbow::CreateDevice(&pDevice);
 	Rainbow::SwapChainDesc swapchainDesc{ pWindow->mWindowHandle, frameCount, w, h, DXGI_FORMAT_R8G8B8A8_UNORM };
 	Rainbow::CreateSwapChain(pDevice, &swapchainDesc, &pSwapChain);
+
+	Rainbow::RenderWindowShow(pWindow);
 	Rainbow::RenderWindowRunLoop(pWindow, Draw);
 
 	Rainbow::RemoveDevice(pDevice);
