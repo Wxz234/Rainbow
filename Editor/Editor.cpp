7df9ad110dc7176150ff4cab18b011b5a626176a
Wxz <429373960@qq.com>
2022-07-01 03:15:44 +0800

    Update Editor.cpp

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -8,6 +8,8 @@
 #include "../Include/Rainbow/Window/Window.h"
 #include "../Include/Rainbow/Renderer/IRenderer.h"
 
+#include "../ThirdParty/DirectXTex/WICTextureLoader/WICTextureLoader12.h"
+#include "../ThirdParty/DirectXTex/WICTextureLoader/d3dx12.h"
 #include <Windows.h>
 #include <d3dcompiler.h>
 #include <string>
@@ -18,6 +20,8 @@ Rainbow::Device* pDevice = nullptr;
 Rainbow::SwapChain* pSwapChain = nullptr;
 ID3D12PipelineState* pipeline = nullptr;
 ID3D12RootSignature* root_signature = nullptr;
+ID3D12Resource* pTex;
+ID3D12DescriptorHeap* pSrv;
 void CreateDefaultPipeline(ID3D12Device* pDevice, ID3D12PipelineState** ppPipeline) {
 
 	D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
@@ -27,7 +31,7 @@ void CreateDefaultPipeline(ID3D12Device* pDevice, ID3D12PipelineState** ppPipeli
 	};
 
 	std::string vs_shader = R"(
-	#define MyRS1 "RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT) "
+	#define MyRS1 "RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT),StaticSampler(s0),DescriptorTable(SRV(t0)) "
 	struct PS_INPUT {
 		float4 position : SV_POSITION;
 		float2 texcoord : TEXCOORD;
@@ -45,9 +49,11 @@ void CreateDefaultPipeline(ID3D12Device* pDevice, ID3D12PipelineState** ppPipeli
 		float4 position : SV_POSITION;
 		float2 texcoord: TEXCOORD;
 	};
+	Texture2D tex : register(t0);
+	SamplerState my_Sampler : register(s0);
 	float4 main(PS_INPUT ps_in) : SV_TARGET
 	{
-		float4 color = float4(0.5,0.5,0.5,1);
+		float4 color = tex.Sample(my_Sampler,ps_in.texcoord);
 		return color;
 	}
 	)";
@@ -96,20 +102,33 @@ void CreateDefaultPipeline(ID3D12Device* pDevice, ID3D12PipelineState** ppPipeli
 	psoDesc.NumRenderTargets = 1;
 	psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
 	psoDesc.SampleDesc.Count = 1;
-	pDevice->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(ppPipeline));
+	auto pp = pDevice->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(ppPipeline));
+
+
 }
+
+bool first = true;
 
 void Draw() {
 	Rainbow::BeginDraw(pDevice, pSwapChain);
+
 	auto mainList = Rainbow::GetSwapChainActiveList(pSwapChain);
+	if (first) {
+		//Rainbow::CmdResourceBarrier(mainList, pTex, D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_COMMON);
+		first = false;
+	}
 	auto rtv = Rainbow::GetSwapChainRTV(pSwapChain);
 	mainList->pDxCmdList->SetPipelineState(pipeline);
 	mainList->pDxCmdList->SetGraphicsRootSignature(root_signature);
+	mainList->pDxCmdList->SetDescriptorHeaps(1, &pSrv);
+	mainList->pDxCmdList->SetGraphicsRootDescriptorTable(0, pSrv->GetGPUDescriptorHandleForHeapStart());
 	mainList->pDxCmdList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	D3D12_VIEWPORT viewport{ 0,0,w,h,0.f,1.f };
 	mainList->pDxCmdList->RSSetViewports(1, &viewport);
 	RECT rect{ 0,0,w,h };
 	mainList->pDxCmdList->RSSetScissorRects(1, &rect);
+
+	
 	mainList->pDxCmdList->OMSetRenderTargets(1, &rtv, 0, 0);
 	mainList->pDxCmdList->DrawInstanced(3, 1, 0, 0);
 	Rainbow::EndDraw(pDevice, pSwapChain);
@@ -144,6 +163,42 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _
 	Rainbow::CreateSwapChain(pDevice, &swapchainDesc, &pSwapChain);
 
 	CreateDefaultPipeline(pDevice->pDxDevice, &pipeline);
+	std::unique_ptr<uint8_t[]> wicData;
+	D3D12_SUBRESOURCE_DATA subresource;
+	DirectX::LoadWICTextureFromFile(pDevice->pDxDevice, L"C:\\Users\\42937\\Documents\\GitHub\\Rainbow\\Asset\\CornellBox.png", &pTex, wicData, subresource);
+
+
+	const UINT64 uploadBufferSize = GetRequiredIntermediateSize(pTex, 0, 1);
+
+	CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_UPLOAD);
+
+	auto desc = CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize);
+
+	// Create the GPU upload buffer.
+	ID3D12Resource* uploadRes;
+
+	pDevice->pDxDevice->CreateCommittedResource(
+			&heapProps,
+			D3D12_HEAP_FLAG_NONE,
+			&desc,
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&uploadRes));
+	Rainbow::CmdReset(pDevice->pCmd);
+	UpdateSubresources(pDevice->pCmd->pDxCmdList, pTex, uploadRes,
+		0, 0, 1, &subresource);
+
+	auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(pTex,
+		D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_COMMON);
+	pDevice->pCmd->pDxCmdList->ResourceBarrier(1, &barrier);
+
+	Rainbow::CmdClose(pDevice->pCmd);
+	Rainbow::QueueExecute(pDevice->pQueue, pDevice->pCmd);
+	Rainbow::QueueWait(pDevice->pQueue);
+
+	D3D12_DESCRIPTOR_HEAP_DESC heapDesc{ D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,1,D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,0 };
+	pDevice->pDxDevice->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&pSrv));
+	pDevice->pDxDevice->CreateShaderResourceView(pTex, nullptr, pSrv->GetCPUDescriptorHandleForHeapStart());
 
 	Rainbow::RenderWindowShow(pWindow);
 	Rainbow::RenderWindowRunLoop(pWindow, Draw);
