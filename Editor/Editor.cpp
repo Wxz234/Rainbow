35c789a744f4cdccfb9df8ff590747c08feba498
Wxz <429373960@qq.com>
2022-06-30 09:39:11 +0800

    remove gui

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -7,10 +7,7 @@
 
 #include "../Include/Rainbow/Window/Window.h"
 #include "../Include/Rainbow/Renderer/IRenderer.h"
-#include "../Include/Rainbow/GUI/GUI.h"
 #include "../Include/Rainbow/Runtime/Scene.h"
-
-#include "../ThirdParty/imgui/imgui.h"
 
 #include <Windows.h>
 
@@ -18,25 +15,15 @@ uint32_t w = 800, h = 600;
 constexpr uint32_t frameCount = 3;
 Rainbow::Device* pDevice = nullptr;
 Rainbow::SwapChain* pSwapChain = nullptr;
-Rainbow::Texture* pTex = nullptr;
-Rainbow::GUI* pGui = nullptr;
 
 void Draw() {
-
-	Rainbow::GUINewFrame(pGui);
-	ImGui::Begin("test");
-	ImGui::End();
 	Rainbow::BeginDraw(pDevice, pSwapChain);
-	Rainbow::GUIDraw(pGui);
 	Rainbow::EndDraw(pDevice, pSwapChain);
 	Rainbow::SwapChainPresent(pSwapChain);
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	if (Rainbow::GUIWndProcHandler(hWnd, message, wParam, lParam)) {
-		return true;
-	}
 	switch (message)
 	{
 	case WM_SIZE:
@@ -61,12 +48,10 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPreInstance, _
 	Rainbow::CreateDevice(&pDevice);
 	Rainbow::SwapChainDesc swapchainDesc{ pWindow->mWindowHandle, frameCount, w, h, DXGI_FORMAT_R8G8B8A8_UNORM };
 	Rainbow::CreateSwapChain(pDevice, &swapchainDesc, &pSwapChain);
-	Rainbow::CreateGUI(pDevice, pSwapChain, &pGui);
 
 	Rainbow::RenderWindowShow(pWindow);
 	Rainbow::RenderWindowRunLoop(pWindow, Draw);
 
-	Rainbow::RemoveGUI(pGui);
 	Rainbow::RemoveSwapChain(pSwapChain);
 	Rainbow::RemoveDevice(pDevice);
 	Rainbow::RemoveRenderWindow(pWindow);
