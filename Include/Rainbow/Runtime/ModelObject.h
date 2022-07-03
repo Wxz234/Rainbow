6f38b4b7f61b65e4142e9fff0e572c7505a7d795
Wxz <429373960@qq.com>
2022-07-03 19:12:59 +0800

    Update ModelObject.h
    ModelObject

--- Include/Rainbow/Runtime/ModelObject.h
+++ Include/Rainbow/Runtime/ModelObject.h
@@ -8,20 +8,29 @@
 #pragma once
 
 #include "GameObject.h"
+#include "MeshComponent.h"
 
-#include <vector>
+#include <memory>
 
 namespace Rainbow {
 	struct ModelObject : public GameObject {
+		ModelObject():mesh(new MeshComponent) {}
+
+		ModelObject(const ModelObject& r) = delete;
+		ModelObject& operator=(const ModelObject& r) = delete;
+		ModelObject(ModelObject&& r) noexcept = default;
+		ModelObject& operator=(ModelObject&& r) noexcept = default;
 
 		virtual bool IsRenderable() { return true; }
 		virtual Component* GetComponent(size_t i) const {
+			if (i == 0) {
+				return mesh.get();
+			}
 			return nullptr;
 		}
-		virtual size_t GetComponentSize() const {
-			return 0;
-		}
+		virtual size_t GetComponentSize() const { return 1; }
 
 	private:
+		std::unique_ptr<MeshComponent> mesh;
 	};
 }
