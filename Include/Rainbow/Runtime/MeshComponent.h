42567bdb65425f19b0762f0d52b169a5c711ae14
Wxz <429373960@qq.com>
2022-07-03 18:51:07 +0800

    Create MeshComponent.h

---
+++ Include/Rainbow/Runtime/MeshComponent.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) Wxz.
+// Licensed under the MIT License.
+//
+// https://github.com/Wxz234/Rainbow
+//
+
+#pragma once
+
+#include "Component.h"
+
+namespace Rainbow {
+	struct MeshComponent : public Component {
+	};
+}
