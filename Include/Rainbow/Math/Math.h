73a13300e5c4ab3756b7729964caf3904b946c40
Wxz <429373960@qq.com>
2022-07-11 18:30:46 +0800

    update matrix

--- Include/Rainbow/Math/Math.h
+++ Include/Rainbow/Math/Math.h
@@ -14,7 +14,6 @@ namespace Rainbow {
     {
         Vector2() { x = y = 0.f; }
         constexpr Vector2(float x, float y) :x(x), y(y) {}
-        
         Vector2(const Vector2&) = default;
         Vector2& operator=(const Vector2&) = default;
         Vector2(Vector2&&) = default;
@@ -28,7 +27,6 @@ namespace Rainbow {
     {
         Vector3() { x = y = z = 0.f; }
         constexpr Vector3(float x, float y, float z) :x(x), y(y), z(z) {}
-
         Vector3(const Vector3&) = default;
         Vector3& operator=(const Vector3&) = default;
         Vector3(Vector3&&) = default;
@@ -39,21 +37,10 @@ namespace Rainbow {
         float z;
     };
 
-    inline float Vector3Dot(const Vector3& a, const Vector3& b) {
-        return a.x * b.x + a.y * b.y + a.z * b.z;
-    }
-
-    inline Vector3 Vector3Normalize(const Vector3& v) {
-        float _len = sqrtf(Vector3Dot(v, v));
-        _len = 1.0f / _len;
-        return Vector3(v.x * _len, v.y * _len, v.z * _len);
-    }
-
     struct Vector4
     {
         Vector4() { x = y = z = w = 0.f; }
         constexpr Vector4(float x, float y, float z, float w) :x(x), y(y), z(z), w(w) {}
-
         Vector4(const Vector4&) = default;
         Vector4& operator=(const Vector4&) = default;
         Vector4(Vector4&&) = default;
--- Runtime/CameraObject.cpp
+++ Runtime/CameraObject.cpp
@@ -4,85 +4,15 @@
 //
 // https://github.com/Wxz234/Rainbow
 //
-#include "../Include/Rainbow/Runtime/CameraObject.h"	
-#include <DirectXMath.h>
-#include <cstring>
+#include "../Include/Rainbow/Runtime/CameraObject.h"
 
 namespace Rainbow {
 
-    inline void _ScalarSinCos
-    (
-        float* pSin,
-        float* pCos,
-        float  Value
-    )
-    {
-        assert(pSin);
-        assert(pCos);
-
-        constexpr float _1DIV2PI = 0.159154943f;
-        constexpr float _2PI = 6.283185307f;
-        constexpr float _PIDIV2 = 1.570796327f;
-        constexpr float _PI = 3.141592654f;
-
-        // Map Value to y in [-pi,pi], x = 2*pi*quotient + remainder.
-        float quotient = _1DIV2PI * Value;
-        if (Value >= 0.0f)
-        {
-            quotient = static_cast<float>(static_cast<int>(quotient + 0.5f));
-        }
-        else
-        {
-            quotient = static_cast<float>(static_cast<int>(quotient - 0.5f));
-        }
-        float y = Value - _2PI * quotient;
-
-        // Map y to [-pi/2,pi/2] with sin(y) = sin(Value).
-        float sign;
-        if (y > _PIDIV2)
-        {
-            y = _PI - y;
-            sign = -1.0f;
-        }
-        else if (y < -_PIDIV2)
-        {
-            y = -_PI - y;
-            sign = -1.0f;
-        }
-        else
-        {
-            sign = +1.0f;
-        }
-
-        float y2 = y * y;
-
-        // 11-degree minimax approximation
-        *pSin = (((((-2.3889859e-08f * y2 + 2.7525562e-06f) * y2 - 0.00019840874f) * y2 + 0.0083333310f) * y2 - 0.16666667f) * y2 + 1.0f) * y;
-
-        // 10-degree minimax approximation
-        float p = ((((-2.6051615e-07f * y2 + 2.4760495e-05f) * y2 - 0.0013888378f) * y2 + 0.041666638f) * y2 - 0.5f) * y2 + 1.0f;
-        *pCos = sign * p;
-    }
-
 	Matrix CameraObject::GetViewMatrix() const {
-
-        Vector3 R2 = Vector3Normalize(dir);
 
 		return Matrix();
 	}
 	Matrix CameraObject::GetPerspectiveMatrix() const {
-        float    SinFov;
-        float    CosFov;
-        _ScalarSinCos(&SinFov, &CosFov, 0.5f * FovAngleY);
-        float Height = CosFov / SinFov;
-        float Width = Height / AspectRatio;
-        float fRange = FarZ / (FarZ - NearZ);
-
-        Matrix M;
-        M.r[0] = Vector4(Width, 0, 0, 0);
-        M.r[1] = Vector4(0, Height, 0, 0);
-        M.r[2] = Vector4(0, 0, fRange, 1);
-        M.r[3] = Vector4(0, 0, -fRange * NearZ, 0);
-		return M;
+		return Matrix();
 	}
 }
